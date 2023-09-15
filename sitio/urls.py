34337e4e20a4d34e3e81bbc255f33b7f363b934b
Francisco <leoneljansen777@gmail.com>
2023-09-15 11:37:07 -0300

    Funcionalidad del login

--- sitio/urls.py
+++ sitio/urls.py
@@ -1,8 +1,13 @@
 from . import views
 from django.urls import path
-
-urlpatterns = [
-    path('', views.index),
-    path('login/', views.login),
-    path('dashboard/', views.dashboard),
+from django.contrib import admin
+from django.contrib.auth.views import LoginView,logout_then_login
+from django.contrib.auth.decorators import login_required
+urlpatterns = [ 
+    path('',login_required(views.index),name='index'),
+    #path('login/', views.login),   
+    path('dashboard/',login_required(views.dashboard),name='dashboard'),
+    path('accounts/login/',LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/',logout_then_login,name='logout')
+    
 ]
--- sitio/views.py
+++ sitio/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from .models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def index (request):
-    user=User.objects.all()
-    return render (request, 'index.html', {
-        'user': user
-    })
-
+    return render (request, 'index.html')
 
 def login (request):
+    
     return render(request, 'login.html') 
 
 def dashboard (request):
     return render(request, 'dashboard.html') 
+
+
