62bd7e36fa8d76ffc866ecc7e6e06bfa4987699f
Francisco <leoneljansen777@gmail.com>
2023-09-15 13:12:09 -0300

    arreglando errores

--- sitio/urls.py
+++ sitio/urls.py
@@ -1,22 +1,13 @@
 from . import views
 from django.urls import path
-<<<<<<< HEAD
 from django.contrib import admin
 from django.contrib.auth.views import LoginView,logout_then_login
 from django.contrib.auth.decorators import login_required
 urlpatterns = [ 
-    path('',login_required(views.index),name='index'),
-    #path('login/', views.login),   
+    path('',login_required(views.index),name='index'),  
     path('dashboard/',login_required(views.dashboard),name='dashboard'),
     path('accounts/login/',LoginView.as_view(template_name='login.html'),name='login'),
-    path('logout/',logout_then_login,name='logout')
-    
-=======
-
-urlpatterns = [
-    path('', views.index),
-    path('login/', views.login),
-    path('dashboard/', views.dashboard),
+    path('logout/',logout_then_login,name='logout'),
     path('calls/', views.calls),
->>>>>>> ad8b05ee93eed7f89d0652f55199327fd3567549
+    
 ]
--- sitio/views.py
+++ sitio/views.py
@@ -12,9 +12,5 @@ def login (request):
 def dashboard (request):
     return render(request, 'dashboard.html') 
 
-<<<<<<< HEAD
-
-=======
 def calls (request):
     return render(request, 'calls.html') 
->>>>>>> ad8b05ee93eed7f89d0652f55199327fd3567549
