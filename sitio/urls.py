e03d3b54299adff54f2425ba578df47917d47b8b
Mateo Rodriguez <mateorodri209@gmail.com>
2023-10-01 22:11:32 -0300

    Borrar areas y users

--- sitio/urls.py
+++ sitio/urls.py
@@ -11,15 +11,12 @@ urlpatterns = [
 
     path('logout/',logout_then_login,name='logout'),
 
-    path('calls/', login_required(views.calls)),
-
-    path('pacientes/', login_required(views.pacientes)),
-
     path('areas/', login_required(views.areas),name='areas'),
     path('agregar_sala/', login_required(views.agregar_sala)),
     path('editar_sala/<int:sala_id>/', login_required(views.editar_sala),name='editar_sala'),
     path('procesar_formulario/', login_required(views.procesar_formulario), name='procesar_formulario'),
 
+    path('pacientes/', login_required(views.pacientes),name ='paciente'),
     path('agregar_paciente/',login_required(views.agregar_paciente),name='agregar_paciente'),
     path('editar_paciente/',login_required(views.editar_paciente),name='editar_paciente'),
     path('perfil_paciente/',login_required(views.perfil_paciente),name='perfil_paciente'),
@@ -43,5 +40,8 @@ urlpatterns = [
     path('historial_clinico/',login_required(views.historial_clinico),name='historial_clinico'),
     path('historial_llamadas/',login_required(views.historial_llamadas),name='historial_llamadas'),
 
+    path('calls/', login_required(views.calls),name='calls'),
     path('atender_calls/',login_required(views.atender_calls),name='atender_calls'),
+
+    path('eliminar_objeto/<modelo>/<int:objeto_id>/',login_required(views.eliminar_objeto), name='eliminar_objeto')
 ]
