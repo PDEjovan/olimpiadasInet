a3e6b6f359fce0921d748c03e07e0e492ffad63e
PDEjovan <est.martinovich.jovan@latecnicalf.com.ar>
2023-10-03 02:39:18 -0300

    a

--- sitio/urls.py
+++ sitio/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView,logout_then_login
 from django.contrib.auth.decorators import login_required
 urlpatterns = [ 
-    path('',login_required(views.index),name='index'),  
+    path('',login_required(views.calls),name='calls'),  
     path('dashboard/',login_required(views.dashboard),name='dashboard'),
 
     path('accounts/login/',LoginView.as_view(template_name='login.html'),name='login'),
